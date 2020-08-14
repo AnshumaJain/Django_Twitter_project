3f2074c2e8e7b81257c5f35c3204477bf72d825c
Anshuma Jain <anshumajain9@gmail.com>
2020-08-14 14:49:54 -0700

    created another copy

--- django_project/settings copy.py
+++ django_project/settings copy.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*yzkmmnpzsw4o=nqr68-vgpk(ilzp+tfvr2xb*t2mbk%4h*1qt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
